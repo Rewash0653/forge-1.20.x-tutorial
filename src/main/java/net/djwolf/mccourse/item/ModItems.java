06c47bcb5e162503d79f871ac2a1f010aac5d942
Rewash0653 <djwolf09prive@gmail.com>
2024-05-02 22:54:06 +0200

    added kohlrabi and peat brick

--- src/main/java/net/djwolf/mccourse/item/ModItems.java
+++ src/main/java/net/djwolf/mccourse/item/ModItems.java
@@ -1,6 +1,7 @@
 package net.djwolf.mccourse.item;
 
 import net.djwolf.mccourse.MCCourseMod;
+import net.djwolf.mccourse.block.custom.FuelItem;
 import net.djwolf.mccourse.item.custom.MetalDetectorItem;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -17,8 +18,14 @@ public class ModItems {
 
     public static final RegistryObject<Item> RAW_ALEXANDRITE = ITEMS.register("raw_alexandrite",
             () -> new Item(new Item.Properties()));
+
     public static final RegistryObject<Item> METAL_DETECTOR = ITEMS.register("metal_detector",
             () -> new MetalDetectorItem(new Item.Properties().durability(100)));
+
+    public static final RegistryObject<Item> KOHLRABI = ITEMS.register("kohlrabi",
+            () -> new Item(new Item.Properties().food(ModFoodProperties.KOHLRABI)));
+    public static final RegistryObject<Item> PEAT_BRICK = ITEMS.register("peat_brick",
+            () -> new FuelItem(new Item.Properties(), 200));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
